b229248b35f7c0ade508d3abd81a66c834fd9778
yuri-aes <yuri.nishigaki@denso-ten.com>
2021-05-27 10:03:16 +0900

    test2

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import os, flask
 PORT = int(os.environ['PORT'])
 app = flask.Flask('app server')
-@app.route('/')
+@app.route('/api/v1/hello')
 def index():
   return 'hello Dockerfile'
 app.run(debug=False, host='0.0.0.0', port=PORT)
